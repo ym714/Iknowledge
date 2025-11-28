ce8108b6fd82d59fa5b0e5ac1737228a6a410883
ym714 <2014motomotomoto@gmail.com>
2025-11-28 17:14:51 +0900

    Remove copyright year from Header and Footer

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -4,7 +4,7 @@ export default function Header() {
     return (
         <header className="fixed top-0 w-full p-6 z-50 font-sans backdrop-blur-sm flex justify-between items-center">
             <Link href="/" className="text-lg font-serif font-bold text-gray-900 hover:text-gray-700 transition-colors">
-                <span>know I edge © 2025</span>           </Link>
+                <span>know I edge</span>           </Link>
 
         </header>
     );
